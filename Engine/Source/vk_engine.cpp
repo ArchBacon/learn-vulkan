50d125a2ca7dafdd64f2056f478be523618d56d9
Christian <c.delicaat@hotmail.com>
2024-04-05 23:53:03 +0200

    Created swapchain

--- Engine/Source/vk_engine.cpp
+++ Engine/Source/vk_engine.cpp
@@ -95,11 +95,11 @@ void VulkanEngine::InitVulkan()
 
     // Make the vulkan instance, with basic debug features
     auto result = builder
-                  .set_app_name("Example Vulkan Application")
-                  .request_validation_layers(bUseValidationLayers)
-                  .use_default_debug_messenger()
-                  .require_api_version(1, 3, 0)
-                  .build();
+        .set_app_name("Example Vulkan Application")
+        .request_validation_layers(bUseValidationLayers)
+        .use_default_debug_messenger()
+        .require_api_version(1, 3, 0)
+        .build();
 
     vkb::Instance vkbInstance = result.value();
 
@@ -123,17 +123,17 @@ void VulkanEngine::InitVulkan()
     // We want a GPU that can write tot he SDL surface and supports vulkan 1.3 with the correct features
     vkb::PhysicalDeviceSelector selector{vkbInstance};
     vkb::PhysicalDevice physicalDevice = selector
-                                         .set_minimum_version(1, 3)
-                                         .set_required_features_13(features13)
-                                         .set_required_features_12(features12)
-                                         .set_surface(surface)
-                                         .select()
-                                         .value();
+        .set_minimum_version(1, 3)
+        .set_required_features_13(features13)
+        .set_required_features_12(features12)
+        .set_surface(surface)
+        .select()
+        .value();
 
     // Create the final vulkan device
     vkb::DeviceBuilder deviceBuilder{physicalDevice};
     vkb::Device vkbDevice = deviceBuilder.build().value();
-    
+
     // Get the VkDevice handle used in the rest of a vulkan application
     device = vkbDevice.device;
     chosenGPU = physicalDevice.physical_device;
@@ -141,7 +141,7 @@ void VulkanEngine::InitVulkan()
 
 void VulkanEngine::InitSwapchain()
 {
-    // nothing yet
+    CreateSwapchain(windowExtent.width, windowExtent.height);
 }
 
 void VulkanEngine::InitCommands()
@@ -152,4 +152,38 @@ void VulkanEngine::InitCommands()
 void VulkanEngine::InitSyncStructures()
 {
     // nothing yet
+}
+
+void VulkanEngine::CreateSwapchain(uint32_t width, uint32_t height)
+{
+    vkb::SwapchainBuilder swapchainBuilder{chosenGPU, device, surface};
+
+    swapchainImageFormat = VK_FORMAT_B8G8R8A8_UNORM;
+
+    vkb::Swapchain vkbSwapchain = swapchainBuilder
+        //.use_default_format_selection()
+        .set_desired_format(VkSurfaceFormatKHR{swapchainImageFormat, VK_COLOR_SPACE_SRGB_NONLINEAR_KHR})
+        // Use vsync present mode
+        .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+        .set_desired_extent(width, height)
+        .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+        .build()
+        .value();
+
+    swapchainExtend = vkbSwapchain.extent;
+    // Store swapchain and its related images
+    swapchain = vkbSwapchain.swapchain;
+    swapchainImages = vkbSwapchain.get_images().value();
+    swapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+void VulkanEngine::DestroySwapchain()
+{
+    vkDestroySwapchainKHR(device, swapchain, nullptr);
+
+    // Destroy swapchain resources
+    for (const auto& swapchainImageView : swapchainImageViews)
+    {
+        vkDestroyImageView(device, swapchainImageView, nullptr);
+    }
 }
--- Engine/Source/vk_engine.h
+++ Engine/Source/vk_engine.h
@@ -37,9 +37,19 @@ public:
     VkDevice device; // Vulkan device for commands
     VkSurfaceKHR surface; // Vulkan window surface
 
+    VkSwapchainKHR swapchain;
+    VkFormat swapchainImageFormat;
+
+    std::vector<VkImage> swapchainImages;
+    std::vector<VkImageView> swapchainImageViews;
+    VkExtent2D swapchainExtend;
+
 private:
     void InitVulkan();
     void InitSwapchain();
     void InitCommands();
     void InitSyncStructures();
+
+    void CreateSwapchain(uint32_t width, uint32_t height);
+    void DestroySwapchain();
 };
